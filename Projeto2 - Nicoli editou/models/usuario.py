143c6c78e52e36f8d8404a2413996416854afc3e
Fabricio <10428537@mackenzista.com.br>
2025-08-25 20:27:15 -0300

    Delete Projeto2 - Nicoli editou directory

--- Projeto2 - Nicoli editou/models/usuario.py
+++
@@ -1,8 +0,0 @@
-# Representa a entidade Usuario com os dados do banco
-class Usuario:
-    def __init__(self,nome,email,senha, id=None):
-        self.id=id
-        self.nome = nome
-        self.email = email
-        self.senha = senha
-
